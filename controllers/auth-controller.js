49f0cd840e6081aa822fe3b12cd9b564c357cb8f
Rahul Raj <rahulraaz3142@gmail.com>
2025-04-19 19:19:28 +0530

    Backend initial commit

---
+++ controllers/auth-controller.js
@@ -0,0 +1,71 @@
+const User = require("../models/user-model");
+const bcrypt = require("bcryptjs");
+
+const home = async (req, res) => {
+    try {
+      res.status(200).json({ msg: "Welcome to our home page" });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+
+  const register = async (req, res) => {
+    try {
+      // console.log(req.body);
+
+      const { fullName, username, email, password } = req.body;
+
+      const userExist = await User.findOne({email:email});
+
+      if(userExist) {
+        return res.status(400).json({ message: "email already exists" });
+      }
+
+      // hash the password
+      // const saltRound = 10;
+      // const hash_password = await bcrypt.hash(password, saltRound);
+
+      const userCreated = await User.create({ fullName, username, email, password });
+
+      res.status(201).json({
+        message: "registration successfull", 
+        token: await userCreated.generateToken(), 
+        userId: userCreated._id.toString(),
+      });
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error" });
+    }
+  };
+
+
+  const login = async(req, res) => {
+    try { 
+      const{email, password} = req.body;
+
+      const userExist = await User.findOne({email});
+      console.log(userExist);
+
+      if(!userExist) {
+        return res.status(400).json({message: "Invalid credentials"});
+      }
+
+      // compare password
+      const user = await bcrypt.compare(password, userExist.password);
+
+      if(user) {
+        res.status(200).json({
+          message: "Login successfull", 
+          token: await userExist.generateToken(), 
+          userId: userExist._id.toString(),
+        });
+      } else 
+        res.status(401).json({message: "Invalid email or password"});
+      
+    } catch (error) {
+      res.status(500).json("Internal server error")
+      // next(error);
+    }
+  }
+  
+  module.exports = { home, register, login };
---
+++ server.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const authRoute = require("./router/auth-router");
+const contactRoute = require("./router/contact-router");
+const connectDb = require("./utils/db");
+const errorMiddleware = require("./middlewares/error-middleware");
+
+const cors = require("cors");
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+  credentialS: true,
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/form", contactRoute);
+
+app.use(errorMiddleware);
+
+const PORT = 5000;
+
+connectDb().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server is running at port: ${PORT}`);
+  });
+});
