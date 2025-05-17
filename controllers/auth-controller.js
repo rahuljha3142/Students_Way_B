5d7c874afbd641ad6406067c39c1bf3d2932f41a
Rahul Raj <rahulraaz3142@gmail.com>
2025-05-17 09:58:33 +0530

    userdata

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -69,5 +69,16 @@ const home = async (req, res) => {
       // next(error);
     }
   }
+
+
+const user = async(req, res) => {
+    try {
+      const userData = req.user;
+      console.log(userData);
+      return res.status(200).json({userData});
+    } catch (error) {
+      console.log(`error from the the user route ${error}`)
+    }
+  }
   
-  module.exports = { home, register, login };
+  module.exports = { home, register, login, user };
